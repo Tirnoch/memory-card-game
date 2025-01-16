86d660b8ea6d5ce009d813bbb6f91531b59f4f34
firat <firat.duzgun@hotmail.com>
2025-01-16 15:43:59 +0300

    card component pathing v.2

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState, useRef } from 'react';
 import Header from './components/Header';
 import shuffle from './components/shuffle';
 import URL_ARRAY from './components/URL_ARRAY';
-import Card from './components/CArd';
+import Card from './components/Card';
 
 export default function App() {
   const [game, setGame] = useState({
